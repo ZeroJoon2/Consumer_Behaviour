7ed34225742be6f67d01ee169efcf6d55f43ba73
zerojoon2 <comboy8231@gmail.com>
2025-01-31 01:35:12 +0900

    YJ commit9 20250131

--- YJ/15_final_project_crawling/test.py
+++ YJ/15_final_project_crawling/test.py
@@ -17,40 +17,16 @@ import requests
 
 import time
 
-url = 'https://search.danawa.com/dsearch.php?query=%EC%82%BC%EC%84%B1%EC%A0%84%EC%9E%90+%EA%B0%A4%EB%9F%AD%EC%8B%9Cs24+256gb%2C+%EC%9E%90%EA%B8%89%EC%A0%9C&originalQuery=%EC%82%BC%EC%84%B1%EC%A0%84%EC%9E%90+%EA%B0%A4%EB%9F%AD%EC%8B%9Cs24+256gb%2C+%EC%9E%90%EA%B8%89%EC%A0%9C&checkedInfo=N&volumeType=allvs&page=1&limit=40&sort=opinionDESC&list=list&boost=true&tab=main&addDelivery=N&coupangMemberSort=N&simpleDescOpen=Y&mode=simple&isInitTireSmartFinder=N&recommendedSort=N&defaultUICategoryCode=122515&defaultPhysicsCategoryCode=224%7C48419%7C48829%7C0&defaultVmTab=8&defaultVaTab=2041&isZeroPrice=Y&quickProductYN=N&priceUnitSort=N&priceUnitSortOrder=A'
-target_item = ['S24']
 chrome_option = Options()
 chrome_option.add_experimental_option('detach', True)
 
 driver = webdriver.Chrome(options=chrome_option)
-driver.get(url)
-
 wait = WebDriverWait(driver,10)
 
-wait.until(
-    EC.presence_of_element_located((By.ID, 'paginationArea'))
-)
+urls = [['S24', 'https://search.danawa.com/dsearch.php?query=%EC%82%BC%EC%84%B1%EC%A0%84%EC%9E%90+%EA%B0%A4%EB%9F%AD%EC%8B%9Cs24+256gb%2C+%EC%9E%90%EA%B8%89%EC%A0%9C&originalQuery=%EC%82%BC%EC%84%B1%EC%A0%84%EC%9E%90+%EA%B0%A4%EB%9F%AD%EC%8B%9Cs24+256gb%2C+%EC%9E%90%EA%B8%89%EC%A0%9C&checkedInfo=N&volumeType=allvs&page=1&limit=40&sort=opinionDESC&list=list&boost=true&tab=main&addDelivery=N&coupangMemberSort=N&simpleDescOpen=Y&mode=simple&isInitTireSmartFinder=N&recommendedSort=N&defaultUICategoryCode=122515&defaultPhysicsCategoryCode=224%7C48419%7C48829%7C0&defaultVmTab=8&defaultVaTab=2041&isZeroPrice=Y&quickProductYN=N&priceUnitSort=N&priceUnitSortOrder=A']
+       , ['아이폰16', 'https://search.danawa.com/dsearch.php?query=%EC%95%84%EC%9D%B4%ED%8F%B016+%EC%9E%90%EA%B8%89%EC%A0%9C']]
 
-# 120개 보기로 바꿈
-Select(driver.find_element(by = By.CSS_SELECTOR, value = '#DetailSearch_Wrapper > div.view_opt > div > select')).select_by_value('120')
-
-header = {'User-Agent': 'Mozila/5.0 (Windows NT 6.3; Trident/7.0; rv:11.0) like Gecko'}
-res = requests.get(url, headers= header)
-soup = BeautifulSoup(res.text, 'html.parser')
-
-
-soup2 = soup.select('#productListArea > div.main_prodlist.main_prodlist_list > ul')
-link_list = []
-for i in range(0, len(soup2[0].select('a', class_ = 'click_log_prod_review_count'))):
-    if soup2[0].select('a', class_ = 'click_log_prod_review_count')[i]['href'] != '#' or '':
-        for target in target_item:
-            if target not in soup2[0].select('a', class_ = 'click_log_prod_review_count')[0].find('img').get('alt'):
-                link_list.append(soup2[0].select('a', class_ = 'click_log_prod_review_count')[i]['href'])
-
-# 최종 리스트
-link_list = [link_list[idx] for idx, link in enumerate(link_list) if 'companyReviewYN=Y' in link_list[idx]]
-
-df = pd.DataFrame(columns = ['scoring', 'market', 'purchasing_date', 'review_title', 'review_content'])
+target_item = [urls[0][0], urls[1][0]]
 
 def save_to_df(tmp_scoring, tmp_market, tmp_purchasing_date, tmp_review_title, tmp_review_content):
     global df
@@ -63,23 +39,24 @@ def save_to_df(tmp_scoring, tmp_market, tmp_purchasing_date, tmp_review_title, t
     print('df에 저장완료!')
     return df
 
-def df_to_file(df):
-    if df.shape[0] >= 5000:
-        df.to_csv('review_data.csv', mode = 'a', index = False, header = False)
-        del df
-        print('df 한번 지워내고, csv로 저장하기 완료 !')
+def click_link(link, idx):
+    idx = int(idx)
+    global target_item
+    driver.get(link)
+    tmp = BeautifulSoup(driver.page_source, 'html.parser')
+    tmp_title = tmp.select('#blog_content > div.summary_info > div.top_summary > h3 > span')[0]
+    if target_item[idx] in tmp_title.text:
+        return 1
+    
     else:
-        pass
+        print('hmm this is error')
+        return 0
 
-
-def click_link():
-    driver.get(link_list[0])
-    review_soup = BeautifulSoup(driver.page_source, 'html.parser').select('#danawa-prodBlog-productOpinion-list-self > div.mall_review > div.area_right')[0]
-    return review_soup
 
 
 # 페이지 리스트 계산
 def calc_page_list(review_soup):
+    global wait
     page_list = []
     wait.until(
                 EC.presence_of_element_located((By.CSS_SELECTOR, '#danawa-prodBlog-companyReview-content-list > div > div > div span'))
@@ -112,59 +89,136 @@ def crawling(driver):
     return scoring, market, purchasing_date, review_title, review_content
 
 
-def repit_page(): 
-    while True:
-        time.sleep(1)
-        wait.until(
-                    EC.presence_of_element_located((By.CSS_SELECTOR, '#danawa-prodBlog-productOpinion-list-self > div.mall_review > div.area_right'))
-        )
-        review_soup = BeautifulSoup(driver.page_source, 'html.parser').select('#danawa-prodBlog-productOpinion-list-self > div.mall_review > div.area_right')[0]
-        
-        page_list = calc_page_list(review_soup)
-        print(page_list)
-        
-        for i in page_list:
-            print(f'{i}페이지 시작합니다.')
-            i = int(i)
-            if i % 10 == 1:
-                tmp_scoring, tmp_market, tmp_purchasing_date, tmp_review_title, tmp_review_content = crawling(driver)
-                save_to_df(tmp_scoring, tmp_market, tmp_purchasing_date, tmp_review_title, tmp_review_content)
-                df_to_file(df)
+def repit_page(isTarget): 
 
-            elif i % 10 >= 2:
+    if isTarget == 1:
+        try :
+            while True:
+                time.sleep(2)
+                wait.until(
+                            EC.presence_of_element_located((By.CSS_SELECTOR, '#danawa-prodBlog-productOpinion-list-self > div.mall_review > div.area_right'))
+                )
+                review_soup = BeautifulSoup(driver.page_source, 'html.parser').select('#danawa-prodBlog-productOpinion-list-self > div.mall_review > div.area_right')[0]
                 
-                wait.until(
-                    EC.presence_of_element_located((By.CSS_SELECTOR, f'a[data-pagenumber="{i}"]'))
-                )
+                page_list = calc_page_list(review_soup)
+                print(page_list)
+                if len(page_list) == 1:
+                    print(f'{page_list[0]}페이지 시작합니다~~')
+                    tmp_scoring, tmp_market, tmp_purchasing_date, tmp_review_title, tmp_review_content = crawling(driver)
+                    save_to_df(tmp_scoring, tmp_market, tmp_purchasing_date, tmp_review_title, tmp_review_content)
+                    break
+                else:
+                    for i in page_list:
+                        print(f'{i}페이지 시작합니다.')
+                        i = int(i)
+                        if i % 10 == 1:
+                            tmp_scoring, tmp_market, tmp_purchasing_date, tmp_review_title, tmp_review_content = crawling(driver)
+                            save_to_df(tmp_scoring, tmp_market, tmp_purchasing_date, tmp_review_title, tmp_review_content)
+                            #df_to_file(df)
 
-                driver.find_element(By.CSS_SELECTOR, f'a[data-pagenumber="{i}"]').click()
-                tmp_scoring, tmp_market, tmp_purchasing_date, tmp_review_title, tmp_review_content = crawling(driver)
-                save_to_df(tmp_scoring, tmp_market, tmp_purchasing_date, tmp_review_title, tmp_review_content)
-                df_to_file(df)
+                        elif i % 10 >= 2:
+                            
+                            wait.until(
+                                EC.presence_of_element_located((By.CSS_SELECTOR, f'a[data-pagenumber="{i}"]'))
+                            )
 
-            elif i % 10 == 0:
-                wait.until(
-                    EC.presence_of_element_located((By.CSS_SELECTOR, f'a[data-pagenumber="{i}"]'))
-                )
+                            driver.find_element(By.CSS_SELECTOR, f'a[data-pagenumber="{i}"]').click()
+                            tmp_scoring, tmp_market, tmp_purchasing_date, tmp_review_title, tmp_review_content = crawling(driver)
+                            save_to_df(tmp_scoring, tmp_market, tmp_purchasing_date, tmp_review_title, tmp_review_content)
+                            #df_to_file(df)
 
-                driver.find_element(By.CSS_SELECTOR, f'a[data-pagenumber="{i}"]').click()
+                        elif i % 10 == 0:
+                            wait.until(
+                                EC.presence_of_element_located((By.CSS_SELECTOR, f'a[data-pagenumber="{i}"]'))
+                            )
 
-                tmp_scoring, tmp_market, tmp_purchasing_date, tmp_review_title, tmp_review_content = crawling(driver)
-                save_to_df(tmp_scoring, tmp_market, tmp_purchasing_date, tmp_review_title, tmp_review_content)
-                df_to_file(df)
-                
-                wait.until(
-                    EC.presence_of_element_located((By.CSS_SELECTOR, '[id^="danawa-pagination-button-next-"] > span'))
-                )
-                driver.find_element(By.CSS_SELECTOR, '[id^="danawa-pagination-button-next-"] > span').click()
+                            driver.find_element(By.CSS_SELECTOR, f'a[data-pagenumber="{i}"]').click()
 
-                print('클릭함')
-        
-        print(df.tail(5))
-        if not driver.find_elements(By.CSS_SELECTOR, '[id^="danawa-pagination-button-next-"] > span'):
-            print("모든 페이지를 처리했습니다.")
-            break
+                            tmp_scoring, tmp_market, tmp_purchasing_date, tmp_review_title, tmp_review_content = crawling(driver)
+                            save_to_df(tmp_scoring, tmp_market, tmp_purchasing_date, tmp_review_title, tmp_review_content)
+                            #df_to_file(df)
+                            
+                            wait.until(
+                                EC.presence_of_element_located((By.CSS_SELECTOR, '[id^="danawa-pagination-button-next-"] > span'))
+                            )
+                            driver.find_element(By.CSS_SELECTOR, '[id^="danawa-pagination-button-next-"] > span').click()
 
-click_link()
-repit_page()
-df.to_csv('test_py.csv', encoding='cp949')
+                            print('클릭함')
+                    
+                            print(df.tail(5))
+                    
+                    if not driver.find_elements(By.CSS_SELECTOR, '[id^="danawa-pagination-button-next-"] > span'):        
+                        for i in page_list:
+                            print(f'{i}페이지 시작합니다~~')
+                            i = int(i)
+                            if i % 10 == 1:
+                                tmp_scoring, tmp_market, tmp_purchasing_date, tmp_review_title, tmp_review_content = crawling(driver)
+                                save_to_df(tmp_scoring, tmp_market, tmp_purchasing_date, tmp_review_title, tmp_review_content)
+                                #df_to_file(df)
+
+                            elif i % 10 >= 2:
+                                
+                                wait.until(
+                                    EC.presence_of_element_located((By.CSS_SELECTOR, f'a[data-pagenumber="{i}"]'))
+                                )
+
+                                driver.find_element(By.CSS_SELECTOR, f'a[data-pagenumber="{i}"]').click()
+                                tmp_scoring, tmp_market, tmp_purchasing_date, tmp_review_title, tmp_review_content = crawling(driver)
+                                save_to_df(tmp_scoring, tmp_market, tmp_purchasing_date, tmp_review_title, tmp_review_content)
+                                #df_to_file(df)
+                            print("마지막 페이지 이제 시작합니다.")
+                            break           
+
+        except TimeoutException as e:
+            print(f'{e}모든 페이지가 끝났습니다.')
+    else:
+        print('target이 아니라서 크롤링하지 않습니다.')
+
+for url in urls:
+    tmp = url
+    url = url[-1]
+    driver.get(url)
+
+    wait.until(
+        EC.presence_of_element_located((By.ID, 'paginationArea'))
+    )
+    
+    # 120개 보기로 바꿈
+    Select(driver.find_element(by = By.CSS_SELECTOR, value = '#DetailSearch_Wrapper > div.view_opt > div > select')).select_by_value('120')
+
+    header = {'User-Agent': 'Mozila/5.0 (Windows NT 6.3; Trident/7.0; rv:11.0) like Gecko'}
+    res = requests.get(url, headers= header)
+    soup = BeautifulSoup(res.text, 'html.parser')
+    soup2 = soup.select('#productListArea > div.main_prodlist.main_prodlist_list > ul')
+    link_list = []
+
+    print('세부 link_list를 추출합니다. 좀 오래 걸리네요(갤럭시 1~2분, 애플 3~5분)')
+    for i in range(0, len(soup2[0].select('a', class_ = 'click_log_prod_review_count'))):
+        if soup2[0].select('a', class_ = 'click_log_prod_review_count')[i]['href'] != '#' or soup2[0].select('a', class_ = 'click_log_prod_review_count')[i]['href'] != '':
+            link_list.append(soup2[0].select('a', class_ = 'click_log_prod_review_count')[i]['href'])
+
+    # 최종 리스트
+    link_list = [link for link in link_list if 'companyReviewYN=Y' in link]
+
+    print(f'''
+          ▶ {url}의 세부 link_list는 다음과 같고 하나씩 추출합니다.
+          ▶ {len(link_list)}개를 추출합니다.
+          ▶ {link_list}
+          ''')
+    
+
+    for idx, link in enumerate(link_list):
+        for i in range(0, 2):
+            df = pd.DataFrame(columns = ['scoring', 'market', 'purchasing_date', 'review_title', 'review_content'])
+            isTarget = click_link(link, i)
+            repit_page(isTarget)
+            
+            if isTarget == 1:
+                df.to_csv(f'danawa_review_{tmp[0]}+{idx}.csv', encoding='utf-8 sig', mode = 'w', index = True, header=True)
+
+            else:
+                print('target item이 아니라, csv 저장도 하지 않습니다.')
+print('★★★★★★★★★★★★★★★★★')
+print('★★★추출 끝!★★★★★★★★')
+print('★★★★★★★★★★★★★★★★★')
+driver.close()
